59e26c74aebaf5b6cca115217096d2d7360c38d6
FlepJack <era.user.ivan@gmail.com>
2022-09-20 21:09:50 +0300

    some styled orderList

--- src/api/gql.ts
+++ src/api/gql.ts
@@ -1,5 +1,4 @@
 import { createClient, fetchExchange } from "urql";
-console.log(process.env.API_URL);
 
 const client = createClient({
   url: process.env.API_URL || "http://localhost:3000/graphql",
